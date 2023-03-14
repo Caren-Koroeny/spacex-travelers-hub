fc0591bd60d760fc85a0e75754e930cebe246be9
Caren-Koroeny <carensiya5@gmail.com>
2023-03-14 14:07:26 +0300

    Add store to index.js file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import store from './redux/store';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <BrowserRouter>
+  <Provider store={store}>
     <App />
+  </Provider>
+  </BrowserRouter>
+   
   </React.StrictMode>,
 );
