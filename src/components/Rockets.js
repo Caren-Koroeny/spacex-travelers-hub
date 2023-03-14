0667c83cfb3d3aebb6ea70d3e5fcc068c83c0a68
shaaibu <shaaibusuleiman@gamil.com>
2023-03-14 17:29:59 +0100

    Render rockets to UI

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,5 +1,30 @@
-const Rockets = () => (
-  <h2> Choose your Rocket</h2>
-);
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRocketData } from '../redux/rockets/rocketSlice';
+import styling from '../styling/Rocket.module.css';
+
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const status = useSelector((state) => state.rockets.status);
+  const rockets = useSelector((state) => state.rockets.rocketListData);
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchRocketData());
+    }
+  }, [status, dispatch]);
+
+  return (
+    <>
+      {rockets.map((rocket) => (
+        <ul key={rocket.id}>
+          <img src={rocket.flickr_images} alt="rocket" className={styling.image} />
+          <li>{rocket.name}</li>
+          <li>{rocket.description}</li>
+        </ul>
+      ))}
+    </>
+  );
+};
 
 export default Rockets;
