00a50cd768dda4dd80d1e9397470e51f11d62025
Caren-Koroeny <carensiya5@gmail.com>
2023-03-14 20:27:30 +0300

    FIx linters errors

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,61 +1,59 @@
-import React from 'react';
-import styles from '../styling/Mission.module.css';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getMission } from '../redux/mission/missionSlice';  
+import styles from '../styling/Mission.module.css';
+import { getMission } from '../redux/mission/missionSlice';
 
 const Mission = () => {
-  const { mission } = useSelector((store) => store.missions)
-  const dispatch =  useDispatch();
+  const { mission } = useSelector((store) => store.missions);
+  const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getMission())
-  }, [dispatch])
+    dispatch(getMission());
+  }, [dispatch]);
 
   return (
     <section className={styles.missionContainer}>
-    <div className={styles.headers}>
-      <div>
-        <h2>Mission</h2>
+      <div className={styles.headers}>
+        <div>
+          <h2>Mission</h2>
+        </div>
+        <div>
+          <h2>Description</h2>
+        </div>
+        <div>
+          <h2>Status</h2>
+        </div>
+        <div />
       </div>
-      <div>
-        <h2>Description</h2>
-      </div>
-      <div>
-        <h2>Status</h2>
-      </div>
-      <div />
-    </div>
-    {
-      mission.map((item) => {
-        return (
-          <div className={styles.missionItems} key={item.mission_id}>
-    <div className={styles.missionHead}>
-      <h3>{item.mission_name}</h3>
-    </div>
-    <div className={styles.missionInfo}>
-      <p>{item.description}</p>
-    </div>
-    <div className={styles.buttons}>
-      <button
-        type="button"
-        className={styles.memberBtn}
-      >
-        NOT A MEMBER
-      </button>
-    </div>
-    <div className={styles.buttons}>
-      <button
-        type="button"
-        className={styles.joinMissionBtn}
-      >
-        Join Mission
-      </button>
-    </div>
-  </div>
-        )
-      })
+      {
+      mission.map((item) => (
+        <div className={styles.missionItems} key={item.mission_id}>
+          <div className={styles.missionHead}>
+            <h3>{item.mission_name}</h3>
+          </div>
+          <div className={styles.missionInfo}>
+            <p>{item.description}</p>
+          </div>
+          <div className={styles.buttons}>
+            <button
+              type="button"
+              className={styles.memberBtn}
+            >
+              NOT A MEMBER
+            </button>
+          </div>
+          <div className={styles.buttons}>
+            <button
+              type="button"
+              className={styles.joinMissionBtn}
+            >
+              Join Mission
+            </button>
+          </div>
+        </div>
+      ))
     }
-  </section>
-  )};
+    </section>
+  );
+};
 
 export default Mission;
