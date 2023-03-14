94e0a9dc721a9973913759922dd048b61ff82396
Caren-Koroeny <carensiya5@gmail.com>
2023-03-14 11:56:00 +0300

    Add mission file

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,5 +1,17 @@
-const Mission = () => (
-  <h1>Mission page</h1>
-);
+import React from 'react';
+import styles from '../styling/Mission.module.css';
+
+const Mission = () => {
+  return (
+    <section className={styles.missionContainer} >
+    <div className={styles.headers}>
+    <h2>Mission</h2>
+    <h2>Description</h2>
+    <h2>Status</h2>
+    </div>
+    
+    </section>
+  )
+  };
 
 export default Mission;
