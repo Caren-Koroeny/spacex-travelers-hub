737d23eb680555537e8427f7ffd55d509d9f63a6
shaaibu <shaaibusuleiman@gamil.com>
2023-03-15 15:47:23 +0100

    Implement rocket actions

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchRocketData } from '../redux/rockets/rocketSlice';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+import { fetchRocketData, reserveRocket } from '../redux/rockets/rocketSlice';
 import styling from '../styling/Rocket.module.css';
 
 const Rockets = () => {
@@ -20,9 +22,20 @@ const Rockets = () => {
         <ul key={rocket.id} className={styling.container}>
           <img src={rocket.flickr_images} alt="rocket" className={styling.image} />
           <div className={styling.contain}>
-            <p className={styling.item}>{rocket.name}</p>
-            <li className={styling.item}>{rocket.description}</li>
-            <button type="button" className={styling.reserve}>Reserve Rocket</button>
+            <p>
+              <Badge bg={rocket.reserved ? ('info') : ('secondary')}>
+                {rocket.reserved ? ('Reserved') : ('')}
+              </Badge>
+              {`${rocket.description}`}
+            </p>
+            <Button
+              type="button"
+              variant={rocket.reserved ? ('outline-secondary') : ('btn btn-primary')}
+              size="md"
+              onClick={() => dispatch(reserveRocket(rocket.id))}
+            >
+              {rocket.reserved ? ('Cancel Reservation') : ('Reserve Rocket')}
+            </Button>
           </div>
         </ul>
       ))}
