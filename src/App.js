a18cb87fab878ebd33239d17a46c198c9e7664b8
Caren-Koroeny <carensiya5@gmail.com>
2023-03-15 20:18:36 +0300

    Add useDispatch in App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter, Routes, Route,
+} from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getMission } from './redux/mission/missionSlice';
 import Profile from './components/Profile';
 import Mission from './components/Mission';
 import Rockets from './components/Rockets';
 import Navigation from './components/Navbar';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMission());
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <Navigation />
