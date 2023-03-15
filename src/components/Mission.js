4ff6310762ac1e5c3d4db130bb44670789a08414
Caren-Koroeny <carensiya5@gmail.com>
2023-03-15 08:48:12 +0300

    Added Some changes by creating a funciton

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -6,9 +6,37 @@ import { getMission } from '../redux/mission/missionSlice';
 const Mission = () => {
   const { mission } = useSelector((store) => store.missions);
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(getMission());
   }, [dispatch]);
+
+  const renderMissionItems = () => mission.map((item) => (
+    <div className={styles.missionItems} key={item.mission_id}>
+      <div className={styles.missionHead}>
+        <h3>{item.mission_name}</h3>
+      </div>
+      <div className={styles.missionInfo}>
+        <p>{item.description}</p>
+      </div>
+      <div className={styles.buttons}>
+        <button
+          type="button"
+          className={styles.memberBtn}
+        >
+          NOT A MEMBER
+        </button>
+      </div>
+      <div className={styles.buttons}>
+        <button
+          type="button"
+          className={styles.joinMissionBtn}
+        >
+          Join Mission
+        </button>
+      </div>
+    </div>
+  ));
 
   return (
     <section className={styles.missionContainer}>
@@ -24,34 +52,7 @@ const Mission = () => {
         </div>
         <div />
       </div>
-      {
-      mission.map((item) => (
-        <div className={styles.missionItems} key={item.mission_id}>
-          <div className={styles.missionHead}>
-            <h3>{item.mission_name}</h3>
-          </div>
-          <div className={styles.missionInfo}>
-            <p>{item.description}</p>
-          </div>
-          <div className={styles.buttons}>
-            <button
-              type="button"
-              className={styles.memberBtn}
-            >
-              NOT A MEMBER
-            </button>
-          </div>
-          <div className={styles.buttons}>
-            <button
-              type="button"
-              className={styles.joinMissionBtn}
-            >
-              Join Mission
-            </button>
-          </div>
-        </div>
-      ))
-    }
+      {renderMissionItems()}
     </section>
   );
 };
