63d2a4264fac9f6eeb0780cb1edba828c45212bf
Caren-Koroeny <carensiya5@gmail.com>
2023-03-16 08:45:26 +0300

    Added Styling on both mission and Rockets containers

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,21 +1,19 @@
 import React from "react";
-import Navigation from "./Navbar";
 
 const Profile = () => {
   return(
     <div className="container">
-    <Navigation />
-    <div className="profile-container">
-    <div className="myMissions">
-    <h2>My Missions</h2>
-    <div className="cardsContainer">{}</div>
+    <div className="profileContainer">
+      <div className="myMissions">
+        <h2>My Missions</h2>
+        <div className="cardsContainer">{}</div>
+      </div>
+      <div className="myRockets">
+        <h2>My Rockets</h2>
+        <div className="cardsContainer">{}</div>
+      </div>
     </div>
-    <div className="myRockets">
-    <h2>My Rockets</h2>
-    <div className="cardsContainer">{rocketsToRender}</div>
   </div>
-    </div>
-    </div>
   );
 }
 
